937affc36b4a1f0c85ef8609be895b322b9e59ff
Valery <tereshonok.v@gmail.com>
2023-06-15 22:44:33 +0300

    task 1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class test {
+  static void Main() 
+  {
+    Console.Write("Input first number ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input second number ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a>b)
+    {
+        Console.WriteLine($" max = {a}"); 
+    }
+    else
+    {
+        Console.WriteLine($" max = {b}");
+    }
+      }
+}

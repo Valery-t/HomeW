9dd41c2ab3ebb8c866683d01de2440b20da8ef7b
Valery-t <65166365+Valery-t@users.noreply.github.com>
2023-06-15 23:33:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+class test {
+  static void Main() 
+  {
+    Console.Write("Input first number ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input second number ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input third number ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    int max = a;
+    if (b>max)
+    {
+        max=b;
+    }
+    if (c>max)
+    {
+        max=c;
+    }
+        Console.WriteLine($" max = {max}");
+    }
+}

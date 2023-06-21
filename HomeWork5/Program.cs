bb1993449f76643da08caa456fb9366a9f49e2eb
Valery <tereshonok.v@gmail.com>
2023-06-21 23:59:33 +0300

    task 10 done

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+
+   int getDigitNum (int num)
+    {
+       while (num>99 && num<1000)
+       {
+           int res = (num%100-num%10)/10;
+           return res;
+        }
+    return 0;
+    }
+    string inputValidation (int n)
+    {
+        if (n>0)
+        {
+            string a = n.ToString();
+            return a;
+        }
+        else 
+        {
+            return "incorrect input";
+        }
+    }
+
+    Console.WriteLine("Enter a three-digit number");
+    int DigitNum = Convert.ToInt32 (Console.ReadLine ());
+    int result = getDigitNum (DigitNum);
+    Console.WriteLine(inputValidation(result));

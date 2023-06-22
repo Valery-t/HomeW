896c317eb42f7c2a65feee5286bdfbbfde0af0eb
Valery <tereshonok.v@gmail.com>
2023-06-22 21:08:32 +0300

    seminar 2 task 2 done

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int getDigitNum (int n)
+    {
+         int result = -1;
+            if (n >99)
+            {
+                while (n > 999)
+                {
+                n =n / 10;
+                }
+                result =n % 10;
+            }
+            return result; 
+    }
+
+    string inputValidation (int n)
+    {
+        if (n!=-1)
+        {
+            string a = n.ToString();
+            return a;
+        }
+        else 
+        {
+            return "the third digit is missing";
+        }
+    }
+
+Console.WriteLine("Enter digit");
+int DigitNum = Convert.ToInt32 (Console.ReadLine ());
+int res = getDigitNum (DigitNum);
+Console.WriteLine(inputValidation(res));

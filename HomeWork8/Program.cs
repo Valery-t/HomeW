ea24893f32ec5ebbfb28e3f1b29e0dab48eac7f2
Valery <tereshonok.v@gmail.com>
2023-06-25 23:14:18 +0300

    sem3 task 19 is done

---
+++ HomeWork8/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+
+
+void Print (string text){
+    Console.WriteLine(text);
+}
+
+int GetIntNum (){
+    int numX = Convert.ToInt32(Console.ReadLine());
+    return numX;
+}
+
+string CheckPolindrom (string digit)
+{
+    for (int i=0; i < digit.Length/2; i++)
+    {
+        if (digit[i] != digit[digit.Length-i-1])
+            {
+                return "no";
+            }
+             
+    }  
+    return "yes";     
+} 
+Print("enter a five-digit number");
+int N = GetIntNum();
+string res = CheckPolindrom($"{N}");
+Print($"{N} -> {res}");

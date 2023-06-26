38e479b51f10d5521d43c0529657a35df837d545
Valery <tereshonok.v@gmail.com>
2023-06-26 23:57:45 +0300

    sem4 task 29 is done

---
+++ HomeWork13/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+void Print (string text){
+    Console.WriteLine(text);
+}
+
+void SetAnArray (int [] arr)
+{
+    int min = 1;
+    int max = 10;
+
+    for (int i=0; i < arr.Length-1;i++){
+        arr[i] = new Random().Next(min,max);
+    }
+    string g = string.Join(", ",arr);
+    Print($"Array => {g}");
+}
+int [] array = new int[8];
+SetAnArray(array);
+
+
+
+

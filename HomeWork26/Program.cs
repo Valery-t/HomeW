00c900067658ef2d6e5502a4a61fe744e0ab34ca
Valery Tereshonok <tereshonok.v@gmail.com>
2023-07-11 12:47:43 +0300

    sem 8 task 60 is done

--- HomeWork26/Program.cs
+++ HomeWork26/Program.cs
@@ -1,7 +1,66 @@
-﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-Массив размером 2 x 2 x 2
-66(0,0,0) 25(0,1,0)
-34(1,0,0) 41(1,1,0)
-27(0,0,1) 90(0,1,1)
-26(1,0,1) 55(1,1,1) */
+﻿/* Задача 60. ...Сформируйте двухмерный массив из неповторяющихся двузначных чисел. 
+Массив размером 4 x 2 
+66 25
+34 41
+27 90
+26 55*/
+
+int [,] CreateArray (int row, int column){
+    int [,] array = new int [row, column];
+    int [] Uarr = CreateRandomArray(10,90);
+    int K = 0;
+    for (int i = 0; i < row; i++)
+    {     
+        for (int j = 0; j < column; j++)
+            {
+            array[i,j] = Uarr[K];
+            ++K;
+            }
+    }
+    return array;
+}
+int[] CreateRandomArray(int minValue, int length)
+    {
+        Random rand = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < length; ++i)
+        {
+            bool isUnique;
+            do
+            {
+                array[i] = rand.Next(minValue, minValue + length);
+                isUnique = true;
+                for (int j = 0; j < i; ++j)
+                    if (array[i] == array[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+            } 
+            while (!isUnique);
+        }
+        return array;
+    }
+void PrintArray (int[,] array){
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+     {
+        Console.Write("[ ");
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{array[i,j]}\t");
+            
+        }  
+        Console.Write("]");
+        Console.WriteLine();    
+    }
+}
+int [,] array = CreateArray(9,10);
+PrintArray(array); 
+
+
+
+
+
 

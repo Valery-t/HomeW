4ceb2062bf9cf524dea9f78fad656707a0245c50
Valery Tereshonok <tereshonok.v@gmail.com>
2023-07-12 22:27:29 +0300

    sem 9 task 64 is done

---
+++ HomeWork27/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+string OutputRealDigit (int Number){
+    if (Number == 1) return Number.ToString();
+    
+    return Number + ", " + OutputRealDigit(Number-1);
+     
+}
+int GetIntNum (){
+    int numX = Convert.ToInt32(Console.ReadLine());
+    return numX;
+}
+void Print (string text){
+    Console.WriteLine(text);
+}
+void PrintAnswer (){
+    Print("Enter a digit number");
+    int N = GetIntNum();
+    Console.WriteLine($"All natural numbers between {N} and 1");
+    Console.WriteLine(OutputRealDigit(N));
+}
+
+PrintAnswer();
